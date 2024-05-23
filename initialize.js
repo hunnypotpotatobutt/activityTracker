6842f5e05bc05c42a996e08a6be11af15cacb1ef
hunnypotpotatobutt <159744208+hunnypotpotatobutt@users.noreply.github.com>
2024-05-23 03:38:06 -0400

    Add files via upload

--- initialize.js
+++ initialize.js
@@ -3,9 +3,9 @@ activityTime = new Array(activities.length);
 lastPicClicked = "none";
 
 
-
 for (var i = 0; i < activities.length; i++) {
-  activityTime[i] = 0;
+  if (localStorage.getItem(activities[i])) {activityTime[i] = parseInt(localStorage.getItem(activities[i]))}
+  else {activityTime[i] = 0;}
   $('activityPicDiv').innerHTML += '<img id="'+activities[i]+'" src="pics/'+activities[i]+'.png" class = "activityPics" onclick="highlight()">'
   if ((i+1)%4 == 0) {$('activityPicDiv').innerHTML += '<br>'}
 }
--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 function update() {
   activityTime[activities.indexOf(lastPicClicked)] += 1;
+  localStorage.setItem(lastPicClicked, activityTime[activities.indexOf(lastPicClicked)])
+  console.log(activityTime)
 }
 
 function pageToggle() {
@@ -8,6 +10,6 @@ function pageToggle() {
     
 function highlight() {
   event.target.style.border = '3px solid yellow';
-  if (lastPicClicked != "none") {$(lastPicClicked).style.border = '3px solid cyan'}
+  if (lastPicClicked != "none") {$(lastPicClicked).style.border = '3px solid gray'}
   lastPicClicked = event.target.id;
 }
