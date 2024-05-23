3f15f73b86379d12bd2225a2f228acab5d367ced
hunnypotpotatobutt <159744208+hunnypotpotatobutt@users.noreply.github.com>
2024-05-23 04:51:23 -0400

    Add files via upload

--- initialize.js
+++ initialize.js
@@ -1,16 +1,11 @@
 
 activities = ["masturbate", "gamesWithBro", "rest", "league", "momma", "parasocial", "sleep", "stats", "webBrowsing"];
 activityTime = new Array(activities.length);
-barColors = new Array(activities.length);
 lastPicClicked = "none";
-
-barColorsPool = ['#e6194b', '#3cb44b', '#ffe119', '#4363d8', '#f58231', '#911eb4', '#46f0f0', '#f032e6', '#bcf60c', '#fabebe', '#008080', '#e6beff', '#9a6324', '#fffac8', '#800000', '#aaffc3', '#808000', '#ffd8b1', '#000075', '#808080', '#ffffff', '#000000']
 
 for (var i = 0; i < activities.length; i++) {
   if (localStorage.getItem(activities[i])) {activityTime[i] = parseInt(localStorage.getItem(activities[i]))}
   else {activityTime[i] = 0;}
-  
-  barColors[i] = barColorsPool[i];
   
   $('activityPicDiv').innerHTML += '<img id="'+activities[i]+'" src="pics/'+activities[i]+'.png" class = "activityPics" onclick="highlight()">'
   if ((i+1)%4 == 0) {$('activityPicDiv').innerHTML += '<br>'}
@@ -18,6 +13,3 @@ for (var i = 0; i < activities.length; i++) {
 
   
 setInterval(update,1000);
-setInterval(chartUpdate,60000);
-
-chartUpdate();
--- main.js
+++ main.js
@@ -1,36 +1,24 @@
 function update() {
-  activityTime[activities.indexOf(lastPicClicked)] += 10;
-  localStorage.setItem(lastPicClicked, activityTime[activities.indexOf(lastPicClicked)])
-  console.log(activityTime)
-}
-
-function chartUpdate() {
-  new Chart($('myChart').getContext('2d'), {
-    type: "pie",
-    data: {
-      labels: activities,
-      datasets: [{
-        backgroundColor: barColors,
-        data: activityTime
-      }]
-    },
-    options: {
-      plugins: {
-        legend: {
-          labelFormat: '{name}:{percentage:.1f}%',
-          itemStyle: {
-            color: 'red',
-            fontSize: '16px'
-          }
-        }
-      }
-    }
-  });
+  activityTime[activities.indexOf(lastPicClicked)] += 1;
+  localStorage.setItem(lastPicClicked, activityTime[activities.indexOf(lastPicClicked)]);
+  console.log(activityTime);
+  
 }
 
 function pageToggle() {
     if ($('activityPicDiv').style.display == "none") $('activityPicDiv').style.display = "inline"
-    else $('activityPicDiv').style.display = "none"
+    else {
+      $('activityPicDiv').style.display = "none";
+      
+      $('statDiv').innerHTML = '';
+      for (var i = 0; i < activities.length; i++) {
+        $('statDiv').innerHTML += activities[i] + ' ' + (activityTime[i]/3600).toFixed(2) + '<br>';
+      }
+    }
+
+
+
+
 }
     
 function highlight() {
